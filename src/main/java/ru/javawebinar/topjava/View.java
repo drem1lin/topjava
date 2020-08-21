f1129214243b501bb4250d89e1c9155d58ee80d6
drem1lin <drem1lin@gmail.com>
2020-08-21 08:58:24 +0300

    lesson 10.5

--- src/main/java/ru/javawebinar/topjava/View.java
+++ src/main/java/ru/javawebinar/topjava/View.java
@@ -1,6 +1,9 @@
 package ru.javawebinar.topjava;
 
+import javax.validation.groups.Default;
+
 public class View {
+    public interface Persist extends Default {}
     public interface JsonREST {}
     public interface JsonUI {}
 }
